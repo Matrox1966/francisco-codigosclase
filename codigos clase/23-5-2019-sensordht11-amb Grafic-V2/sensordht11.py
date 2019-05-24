4e4b56b9ac4f71fcf7ce133a9315cf58601b6ca2
fsanchezmontero <fsanchezmontero@gmail.com>
2019-05-24 10:51:56 +0100

    Prueba Git 240519

---
+++ codigos clase/23-5-2019-sensordht11-amb Grafic-V2/sensordht11.py
@@ -0,0 +1,23 @@
+from time import sleep
+from auxfunc import grafic,lectura
+
+
+
+
+comptador  = 0
+
+temperatura=[]
+humitat=[]
+while True:
+    valors =lectura()
+    temperatura.append(valors[0])
+    humitat.append(valors[1])
+    print(valors,temperatura,humitat)
+    comptador += 1 # comptador = comptador +  1
+    if comptador == 10:
+        grafic(temperatura, humitat) # ara mateix te 10 valors
+        comptador = 0
+        temperatura=[]
+        humitat=[]
+     
+    sleep(1)
